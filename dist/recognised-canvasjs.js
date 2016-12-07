4d6986c17cc4067a7c7951c5487f2703b1185399
Yuki Nishiwaki <uckey.1067@gmail.com>
2016-12-07 00:32:15 +0000

    Fix text canvas problem about strokeStyle not to care

--- dist/recognised-canvasjs.js
+++ dist/recognised-canvasjs.js
@@ -364,11 +364,13 @@ TextCanvasModel.prototype.draw = function(ctx){
   var tmp_fillStyle = ctx.fillStyle
   var tmp_font = ctx.font
   var tmp_lineWidth = ctx.lineWidth
+  var tmp_strokeStyle = ctx.strokeStyle
 
   ctx.globalAlpha = this.globalAlpha
   ctx.fillStyle = this.color
   ctx.font = this.font
   ctx.lineWidth = this.lineWidth
+  ctx.strokeStyle = this.color
 
   if(this.is_bold){
     ctx.strokeText(this.content, this.possitionX, this.possitionY)
@@ -379,6 +381,7 @@ TextCanvasModel.prototype.draw = function(ctx){
   ctx.globalAlpha = tmp_globalAlpha
   ctx.fillStyle = tmp_fillStyle
   ctx.lineWidth = tmp_lineWidth
+  ctx.strokeStyle = tmp_strokeStyle
 }
 TextCanvasModel.prototype.get_px_width = function(){
   return (Number(this.font.match(/([0-9].)px/)[1])/1.7) * this.content.length
